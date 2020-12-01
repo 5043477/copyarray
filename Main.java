89f22598eef7706d5cf6b29b285e9eede4827a98
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-01 16:59:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+
+   int[ ] roster1 = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+   System.out.println(roster1);
+   
+   int[] highScores = new int[14];
+     
+  }
+}
