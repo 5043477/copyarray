af1496cfaf6c65982e384720ce34b78124d015cf
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-01 17:39:40 +0000

    e

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 class Main {
   public static void main(String[] args) {
 
-   int[ ] roster1 = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+   String[ ] roster1 = {"1","2","3","4","5","6","7","8","9","10"};
 
-   System.out.println(roster1);
-   
-   int[] highScores = new int[14];
-     
+    System.out.println("roster 1 has been shown here " +  roster1.length);
   }
 }
