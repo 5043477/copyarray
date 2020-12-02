7b8bfa54e0f03deb2ee8228599250bdea8e28856
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-02 18:05:24 +0000

    6

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
 class Main {
   public static void main(String[] args) {
 
-   String[ ] roster1 = {"1","2","3","4","5","6","7","8","9","10"};
+   String[ ] roster1 = {"0","1","2","3","4","5","6","7","8","9","10"};
 
-    System.out.println("roster 1 has been shown here " +  roster1.length);
+     System.out.println("roster 1 has been shown here ");
+
+     System.out.println(roster1[5]);
+
+    String[ ] roster2 = {"50","11","45","67","54","34","90","80","40","3","6"};
+
+       System.out.println("roster 2 has been shown here");
+
+     System.out.println(roster2[10]);
+
   }
 }
