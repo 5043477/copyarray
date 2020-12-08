0798a8b8d5f1605b5892e521b3a68f2aafd4bc51
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-08 23:48:30 +0000

    i had remade the code and just changed the thing around

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
 class Main {
   public static void main(String[] args) {
+    String[] roster1 = {"chris ", "mike ", "jhon ", "jack ", "bob ", "fredrick ", "joe ", "han ", "jones ", "dasher "};
+      String[] roster2 = new String[10];
 
-   String[ ] roster1 = {"0","1","2","3","4","5","6","7","8","9","10"};
-
-     System.out.println("roster 1 has been shown here ");
-
-     System.out.println(roster1[5]);
-
-    String[ ] roster2 = {"50","11","45","67","54","34","90","80","40","3","6"};
-
-       System.out.println("roster 2 has been shown here");
-
-     System.out.println(roster2[10]);
-
+    for (int i = 3; i < 10; i++)
+        {
+          roster2 = roster1;
+          System.out.println(roster2[5] + "is next to a confession stand.");
+        }
   }
 }
